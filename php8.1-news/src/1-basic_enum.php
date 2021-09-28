de1462477b7204eb96ed9b887fb820792ab3f8b8
Rafa Gómez <rgomezcasas@gmail.com>
2021-09-28 08:37:00 +0200

    feat(php8.1-news): add readonly example

--- php8.1-news/src/1-basic_enum.php
+++ php8.1-news/src/1-basic_enum.php
@@ -14,6 +14,6 @@ function is_good(Editor $editor): string
     return sprintf(random_int(0, 1) === 1 ? $yesMessage : $nopeMessage, $editor->value);
 }
 
-echo is_good(Editor::VSCode)  . "\n";
-echo is_good(Editor::Idea)    . "\n";
+echo is_good(Editor::VSCode) . "\n";
+echo is_good(Editor::Idea)   . "\n";
 echo is_good(Editor::Sublime) . "\n";
---
+++ php8.1-news/src/4-readonly.php
@@ -0,0 +1,30 @@
+<?php
+
+final class UserId
+{
+    public function __construct(public readonly string $value) {}
+}
+
+final class UserFullName
+{
+    public function __construct(public readonly string $name, public readonly string $surname) {}
+}
+
+final class User
+{
+    public function __construct(
+        public readonly UserId $id,
+        public readonly UserFullName $fullName,
+    ) {}
+
+//    public function rename(string $newName):void
+//    {
+//        $this->name = $newName;
+//    }
+}
+
+$user = new User(new UserId("some-id"), new UserFullName("some-name", "some-surname"));
+
+echo $user->id->value . "\n";
+echo $user->fullName->name . "\n";
+echo $user->fullName->surname . "\n";
