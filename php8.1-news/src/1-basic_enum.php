8c3603eacf54286f905ff02d72ae2c3c2bcfb6c7
Rafa Gómez <rgomezcasas@gmail.com>
2021-10-18 13:49:14 +0200

    feat(php8.1-news): improve examples

--- php8.1-news/src/1-basic_enum.php
+++ php8.1-news/src/1-basic_enum.php
@@ -1,19 +1,19 @@
 <?php
 
-enum Editor: string {
-    case VSCode  = 'Visual Studio Code';
-    case Idea    = 'Intellij Idea';
-    case Sublime = 'Sublime Text';
+enum Editor: string
+{
+    case vscode = 'Visual Studio Code';
+    case intellij = 'Intellij';
 }
 
 function is_good(Editor $editor): string
 {
-    $yesMessage  = 'Yeah, %s is good';
-    $nopeMessage = 'Nope, %s is not good';
+    $yesMessage = '%s is a good editor';
+    $noMessage  = '%s is a bad editor';
 
-    return sprintf(random_int(0, 1) === 1 ? $yesMessage : $nopeMessage, $editor->value);
+    return sprintf(random_int(0, 1) === 1 ? $yesMessage : $noMessage, $editor->value);
 }
 
-echo is_good(Editor::VSCode) . "\n";
-echo is_good(Editor::Idea)   . "\n";
-echo is_good(Editor::Sublime) . "\n";
+$editor = Editor::intellij;
+
+echo is_good($editor);
